b18a02450c72c2de13ae0c12344971d2de0641bb
JADLI Aissam <jadliaissam@gmail.com>
2021-01-06 17:27:12 +0100

    recap part2

--- TP8/Exercice Recap/app.js
+++ TP8/Exercice Recap/app.js
@@ -1,27 +1,30 @@
 const express = require('express')
 const bodyParser= require('body-parser')
+var multer = require('multer')
+var upload = multer({dest:'uploads'})
 const app = express()
 var products=[
     {id:1,title:"ordinteur",prix:4000},
     {id:2,title:"Telephone",prix:5000},
     {id:3,title:"Imprimante",prix:2000}
 ]
-
+app.use(express.static('images'));
+app.use(express.static('assets'));
 app.get('/products',(req,res)=>{
     res.render('list.ejs',{
         products:products
     })
-
 })
 app.get('/products/new',(req,res)=>{
     return res.render('new.pug');
 }) 
 app.get('/products/:id',(req,res)=>{
-    const prod=products.filter(p=>p.id===req.params.id)[0];
+    const prod=products.filter(p=>p.id==req.params.id)[0];
     return res.render('detail.pug',{product:prod});
 })
 app.use(bodyParser.urlencoded({extended:false}))
-app.post('/products',(req,res)=>{
+app.post('/products',upload.single('image'),(req,res)=>{
+    console.log(req.file)
     products.push(req.body)
     res.redirect('/products')
 })
--- TP8/Routage/router.js
+++ TP8/Routage/router.js
@@ -2,7 +2,15 @@ var express = require('express')
 
 var router = express.Router()
 
-router.get('/about', (req, res)=>{
+router.get('/', (req, res)=>{
+    res.send('About')
+})
+
+router.get('/:id', (req, res)=>{
+    res.send('About')
+})
+
+router.post('/', (req, res)=>{
     res.send('About')
 })
 
